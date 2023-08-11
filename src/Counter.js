3b9d0254a25e45f0637e2cd5d12dc2ba55df44aa
oakekywe <oakekywe.zotefamily@gmail.com>
2023-08-11 12:48:15 +0630

    [modified] button size and hard reset

--- src/Counter.js
+++ src/Counter.js
@@ -31,6 +31,12 @@ const Counter = () => {
 
   const handleReset = () => {
     setCount(0);
+    // setTargetCount(0);
+    setIsComplete(false);
+  };
+
+  const handleHardReset = () => {
+    setCount(0);
     setTargetCount(0);
     setIsComplete(false);
   };
@@ -61,42 +67,44 @@ const Counter = () => {
   }
 
   return (
-    <div className="flex flex-col items-center p-4">
-      <h1 className="text-2xl font-bold mt-4 mb-8">Counter App</h1>
-      {isComplete && (
-        <div className="bg-green-200 text-green-800 p-4 my-3 rounded">
-          Completed! <button onClick={handleReset}>Start new</button>
+    <div className="flex justify-center items-center p-4 min-h-screen">
+      <div className="flex flex-col justify-center items-center">
+        <h1 className="text-2xl font-bold mt-4 mb-8">Counter App</h1>
+        {isComplete && (
+          <div className="bg-green-200 text-green-800 p-4 my-3 rounded">
+            Completed! <button onClick={handleHardReset}>Start new</button>
+          </div>
+        )}
+        <div className="flex items-center my-4">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold  py-6 px-8 rounded"
+            onClick={handleDecrement}
+          >
+            -
+          </button>
+          <span className="mx-6 text-xl font-bold">{count}</span>
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-6 px-8 rounded"
+            onClick={handleIncrement}
+          >
+            +
+          </button>
         </div>
-      )}
-      <div className="flex items-center my-4">
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold  py-4 px-6 rounded"
-          onClick={handleDecrement}
-        >
-          -
-        </button>
-        <span className="mx-6 text-xl font-bold">{count}</span>
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-4 px-6 rounded"
-          onClick={handleIncrement}
-        >
-          +
-        </button>
-      </div>
-      <div className="flex flex-col items-start mt-5">
-        <input
-          type="number"
-          className="border border-gray-400 rounded py-2 px-4 mb-4"
-          placeholder="Set target count"
-          onChange={handleSetTargetCount}
-          value={targetCount}
-        />
-        <button
-          className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
-          onClick={handleReset}
-        >
-          Reset
-        </button>
+        <div className="flex flex-col mt-5">
+          <input
+            type="number"
+            className="border border-gray-400 rounded py-2 px-4 mb-4"
+            placeholder="Set target count"
+            onChange={handleSetTargetCount}
+            value={targetCount}
+          />
+          <button
+            className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+            onClick={handleReset}
+          >
+            Reset
+          </button>
+        </div>
       </div>
       <audio ref={buttonClickAudioRef} src={buttonClickSound} />
       <audio ref={completionAudioRef} src={completionSound} />
