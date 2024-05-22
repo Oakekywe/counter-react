0f1f91b38a8dce525a008715678571d44e8c2659
oakekywe <oakekywe.zotefamily@gmail.com>
2024-05-22 11:02:47 +0630

    [add] new auto play counting feature

--- src/Counter.js
+++ src/Counter.js
@@ -1,6 +1,7 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import buttonClickSound from "./sound/button-click.mp3";
 import completionSound from "./sound/completion-sound.mp3";
+import { FaPlay, FaStop } from "react-icons/fa";
 
 const Counter = () => {
   const [count, setCount] = useState(0);
@@ -8,6 +9,9 @@ const Counter = () => {
   const [isComplete, setIsComplete] = useState(false);
   const buttonClickAudioRef = useRef(null);
   const completionAudioRef = useRef(null);
+
+  const [isPlay, setIsPlay] = useState(false);
+  const [intervalDuration, setIntervalDuration] = useState(400);
 
   const handleIncrement = () => {
     if (count < targetCount || targetCount === 0) {
@@ -31,7 +35,6 @@ const Counter = () => {
 
   const handleReset = () => {
     setCount(0);
-    // setTargetCount(0);
     setIsComplete(false);
   };
 
@@ -61,15 +64,33 @@ const Counter = () => {
   };
 
   // Check if the count matches the target count
-  if (count === targetCount && targetCount !== 0 && !isComplete) {
-    setIsComplete(true);
-    playSound(completionAudioRef.current);
-  }
+  useEffect(() => {
+    if (count === targetCount && targetCount !== 0 && !isComplete) {
+      setIsComplete(true);
+      setIsPlay(false);
+      playSound(completionAudioRef.current);
+    }
+  }, [count, targetCount, isComplete]);
+
+  // Play
+  const onPressPlay = () => {
+    setIsPlay((prev) => !prev);
+  };
+
+  useEffect(() => {
+    let intervalId;
+    if (isPlay) {
+      intervalId = setInterval(handleIncrement, intervalDuration); // Run handleIncrement every 0.5 seconds
+    } else if (!isPlay && intervalId) {
+      clearInterval(intervalId);
+    }
+    return () => clearInterval(intervalId); // Cleanup interval on component unmount or when isPlay changes
+  }, [isPlay]);
 
   return (
     <div className="flex justify-center bg-gray-800 items-center p-4 min-h-screen">
       <div className="flex flex-col justify-center items-center">
-        <h1 className="text-2xl text-white font-bold mt-4 mb-8">Counter App</h1>
+        <h1 className="text-2xl text-white font-bold mt-4 mb-8">COUNTER</h1>
         {isComplete && (
           <div className="bg-green-200 text-green-800 p-4 my-3 rounded">
             Completed! <button onClick={handleHardReset}>Start new</button>
@@ -78,13 +99,15 @@ const Counter = () => {
         <div className="flex items-center my-4">
           <button
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold  py-6 px-8 rounded"
+            disabled={isPlay}
             onClick={handleDecrement}
           >
             -
           </button>
-          <span className="mx-6 text-xl text-white font-bold">{count}</span>
+          <span className="mx-8 text-xl text-white font-bold">{count}</span>
           <button
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-6 px-8 rounded"
+            disabled={isPlay}
             onClick={handleIncrement}
           >
             +
@@ -97,13 +120,76 @@ const Counter = () => {
             placeholder="Set target count"
             onChange={handleSetTargetCount}
             value={targetCount}
+            disabled={isPlay}
           />
           <button
             className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+            disabled={isPlay}
             onClick={handleReset}
           >
             Reset
           </button>
+
+          <div className="my-6">
+            <div className="my-6">
+              <button
+                className={`${
+                  intervalDuration === 400
+                    ? "bg-gray-700"
+                    : "bg-gray-500 hover:bg-gray-700"
+                } text-white py-2 px-4 rounded mr-1`}
+                disabled={isPlay}
+                onClick={() => setIntervalDuration(400)}
+              >
+                0.4 sec
+              </button>
+              <button
+                className={`${
+                  intervalDuration === 600
+                    ? "bg-gray-700"
+                    : "bg-gray-500 hover:bg-gray-700"
+                } text-white py-2 px-4 rounded mx-1`}
+                disabled={isPlay}
+                onClick={() => setIntervalDuration(600)}
+              >
+                0.6 sec
+              </button>
+              <button
+                className={`${
+                  intervalDuration === 1000
+                    ? "bg-gray-700"
+                    : "bg-gray-500 hover:bg-gray-700"
+                } text-white py-2 px-4 rounded ml-1`}
+                disabled={isPlay}
+                onClick={() => setIntervalDuration(1000)}
+              >
+                1 sec
+              </button>
+            </div>
+            <div className="my-6 flex justify-between items-center">
+              <button
+                className={`${
+                  intervalDuration === 1500
+                    ? "bg-gray-700"
+                    : "bg-gray-500 hover:bg-gray-700"
+                } text-white py-2 px-4 rounded mr-1`}
+                disabled={isPlay}
+                onClick={() => setIntervalDuration(1500)}
+              >
+                1.5 sec
+              </button>
+              <button
+                className={`${
+                  !isPlay
+                    ? "bg-blue-500 hover:bg-blue-700"
+                    : "bg-red-500 hover:bg-red-700"
+                } text-white py-4 px-5 rounded`}
+                onClick={onPressPlay}
+              >
+                {!isPlay ? <FaPlay /> : <FaStop />}
+              </button>
+            </div>
+          </div>
         </div>
       </div>
       <audio ref={buttonClickAudioRef} src={buttonClickSound} />
